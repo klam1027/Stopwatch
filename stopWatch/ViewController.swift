d2e824a0582f20c1a4a9ba25f21ad2d239e21b83
VMassarelli <student@vmassarelli.local>
2016-03-11 11:05:11 -0600

    update 1.0

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -8,18 +8,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var timerlabel: UILabel!
+   
+    var counter = 0
+    var min = 0
+    var hour = 0
+    var timer = NSTimer()
+    var pressed = false
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func startStopTapped(sender: AnyObject)
+    {
+       if pressed == false
+       {
+            startStopButton.setTitle("Stop", forState: UIControlState.Normal)
+            startStopButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        
+            timer.invalidate()
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "timerAction", userInfo: nil, repeats: true)
+            pressed = true
+            
+        }
+        else
+       {
+            timer.invalidate()
+            pressed = false
+            startStopButton.setTitle("Start", forState: UIControlState.Normal)
+            startStopButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+        }
     }
 
-
+    
+    @IBAction func resetTapped(sender: AnyObject)
+    {
+        timer.invalidate()
+        counter = 0
+        min = 0
+        hour = 0
+        timerlabel.text = "00:00.00"
+        pressed = false
+    }
+    
+    func timerAction()
+    {
+        counter++
+        rollover()
+        time()
+    }
+    
+    func rollover()
+    {
+        if counter > 59
+        {
+            counter = 0
+            min++
+        }
+        if min > 59
+        {
+            counter = 0
+            min = 0
+            hour++
+        }
+    }
+    
+    func time()
+    {
+        if hour < 10 && min < 10 && counter < 10
+        {
+            timerlabel.text = "0\(hour):0\(min).0\(counter)"
+        }
+        if hour < 10 && min < 10 && counter >= 10
+        {
+            timerlabel.text = "0\(hour):0\(min).\(counter)"
+        }
+        if hour < 10 && min >= 10 && counter < 10
+        {
+            timerlabel.text = "0\(hour):\(min).0\(counter)"
+        }
+        if hour >= 10 && min < 10 && counter < 10
+        {
+            timerlabel.text = "\(hour):0\(min).0\(counter)"
+        }
+        if hour < 10 && min >= 10 && counter >= 10
+        {
+            timerlabel.text = "0\(hour):\(min).\(counter)"
+        }
+        if hour >= 10 && min < 10 && counter >= 10
+        {
+            timerlabel.text = "\(hour):0\(min).\(counter)"
+        }
+        if hour >= 10 && min >= 10 && counter < 10
+        {
+            timerlabel.text = "\(hour):\(min).0\(counter)"
+        }
+    }
+    
 }
 
